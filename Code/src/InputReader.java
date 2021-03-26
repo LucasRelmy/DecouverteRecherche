a9eb31a6d0dcbacc1be1c2811215b9ac88f93e76
lucasrelmy <lucasrelmy@gmail.com>
2021-03-26 11:58:51 +0100

    Fixed Reader Added Main Class

--- Code/src/InputReader.java
+++ Code/src/InputReader.java
@@ -1,20 +1,27 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class InputReader {
-    public static void main(String[] args) {
+    public List<Element> getData(String path) {
+
+        List<Element> liste = new ArrayList<Element>();
         try {
-            File myObj = new File("C:\\Users\\lucas\\IdeaProjects\\DecouverteRecherche\\Code\\Data\\data1.text");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+            File myObj = new File(path);
+            Scanner scanner = new Scanner(myObj);
+            while (scanner.hasNextLine()) {
+                Element e = new Element();
+                e.setWeight(scanner.nextInt());
+                e.setPrice(scanner.nextInt());
+                liste.add(e);
             }
-            myReader.close();
+            scanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        return liste;
     }
 }
---
+++ Code/src/MainClass.java
@@ -0,0 +1,5 @@
+public class MainClass {
+    public static void main(String[] args) {
+        String path = "C:\\Users\\lucas\\IdeaProjects\\DecouverteRecherche\\Code\\Data\\data1.txt";
+    }
+}
