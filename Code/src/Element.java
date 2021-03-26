026201b2bc7e7684f9e49cd9a4a70a4979f3bb3f
lucasrelmy <lucasrelmy@gmail.com>
2021-03-26 10:51:26 +0100

    Added Reader an element class

---
+++ Code/src/Element.java
@@ -0,0 +1,19 @@
+public class Element {
+    private int price,weight;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
